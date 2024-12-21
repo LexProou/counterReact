8710bf6a0cc1e02afc6bf708b568d0815386a373
Alexander <121025618+LexProou@users.noreply.github.com>
2024-12-21 14:03:07 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducer.jsx'
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
- 
-      <App />
-   
+    <Provider store={store}>
+        <App />
+    </Provider>   
   </StrictMode>,
 )
+
