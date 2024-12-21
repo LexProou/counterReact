fe4bd5a22f79c5c43604e11b812612f7084f29d8
AlexProou <121025618+AlexProou@users.noreply.github.com>
2024-12-21 12:59:49 +0300

    counterReact

---
+++ src/App.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react"
+
+import './App.css'
+
+function App() {
+  // Counter
+  const [count, setCount] = useState(0)
+
+  const inc = () => {
+    setCount( count + 1)
+  }
+
+  const dec = () => {
+    setCount( count - 1 )
+  }
+
+  return(
+    <>
+    <div className="App">
+      <h1>Counter:{count}</h1>
+      <button onClick={inc}>Increment</button>
+      <button onClick={dec}>Decrement</button>
+    </div>
+    </>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // import { useState } from 'react'
+// import { useSelector, useDispatch } from 'react-redux'
+// import { inc, dec } from './actions'
+
+// import './App.css'
+
+// function App() {
+
+//   // const [count, setCount] = useState(0);
+//   const counter = useSelector((state) => state.counter);
+//   const dispatch = useDispatch();
+
+//   // const inc = () => {
+//   //   setCount(count + 1)
+//   // }
+
+//   // const dec = () => {
+//   //   setCount(count - 1)
+//   // }
+
+//   return (
+//     <>
+//      <div className="App">
+//       <h1>Counter: {counter}</h1>
+//       <button onClick={() => dispatch(inc())}>Increment</button>
+//       <button onClick={() => dispatch(dec())}>Decrement</button>
+//      </div>
+       
+//     </>
+//   )
+// }
+
+// export default App
